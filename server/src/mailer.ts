cb4e6c114e30d68e0669cb05afc06ac926f4b7f8
khaidz <49480545+khaidz@users.noreply.github.com>
2023-05-07 21:48:52 +0700

    change

--- server/src/mailer.ts
+++ server/src/mailer.ts
@@ -3,7 +3,7 @@ import nodeMailer from 'nodemailer'
 // Những thông tin dưới đây các bạn có thể ném nó vào biến môi trường .env nhé.
 // Vì để demo nên mình để các biến const ở đây.
 const adminEmail = "khaibq1.vss@gmail.com"; // chỉnh sửa lại đúng với thực tế
-const adminPassword = "yartvbgjlbawgeuy"; // chỉnh sửa lại đúng với thực tế
+const adminPassword = "yartvbgjlbawgeuxy"; // chỉnh sửa lại đúng với thực tế
 // Mình sử dụng host của google - gmail
 const mailHost = 'smtp.gmail.com'
 // 587 là một cổng tiêu chuẩn và phổ biến trong giao thức SMTP
